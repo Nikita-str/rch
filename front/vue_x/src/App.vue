8deb66d95d92663b3bec1716a0a6e9d0900e2d68
Nikita-str <newmynamespace@gmail.com>
2023-04-23 19:01:53 +0300

    it +- work(BUT it called twice: need `const`?)

--- front/vue_x/src/App.vue
+++ front/vue_x/src/App.vue
@@ -1,23 +1,29 @@
 <script>
 export default {
-  methods: {
-    upd(){
-      this.$store.dispatch('updCommonInfo')
-    }
-  },
-  computed: {
-    calcedTotalPost: {
-      get() {
-        this.upd()
-        return this.$store.getters.getTotalPost
-      },
-    },
-  },
   data(){
     return{
+      _updCaled: false,
       openB: 42,
       total_post: this.$store.getters.getTotalPost,
     }
+  },
+  methods: {
+    upd(){
+      console.log("upd")
+      this.$store.dispatch('updCommonInfo')
+    },
+    calcTotalPostFn() {
+        console.log("upd by fn")
+        this.upd()
+        return this.$store.getters.getTotalPost
+    },
+  },
+  computed: {
+    calcTotalPost() {
+        console.log("upd by computed")
+        this.upd()
+        return this.$store.getters.getTotalPost
+    },
   },
 }
 </script>
@@ -27,13 +33,12 @@ import HelloWorld from './components/HelloWorld.vue'
 import About from './components/About.vue'
 </script>
 
-
 <template>
   <div>
     <div style="height: 100%;">
       <div id="center">
         <HelloWorld msg="Незабывайте спать" />
-        <About msg="Ура-ура общение!" :open_boards="openB" :posting_speed="0" :total_post="calcedTotalPost" />
+        <About msg="Ура-ура общение!" :open_boards="openB" :posting_speed="0" :total_post="calcTotalPostFn()" />
         <router-link to="/tmp">X</router-link>
       </div>
     </div>
