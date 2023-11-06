2fcd9987d451577c368ccbd41cd943acccb07c75
Nikita-str <newmynamespace@gmail.com>
2023-11-06 20:32:31 +0300

    #5 [back] test: auto thr creator

---
+++ back/server/test/REST/create_b_threads.py
@@ -0,0 +1,20 @@
+import http.client
+
+conn = http.client.HTTPConnection("127.0.0.1:5173")
+
+headers = {
+    'user-agent': "vscode-restclient",
+    'content-type': "application/json;charset=UTF-8"
+}
+1
+###
+
+for i in range(0, 25):
+    ix = str(i + 1)
+    ii = ix * (i + 1)
+    payload = f"{{\"board_url\":\"b\", \"post_header\":\"Thread#{ix}\", \"post_text\":\"{ix}:{ii}\"}}"
+    conn.request("POST", "/api/board/thr_new", payload.encode('utf-8'), headers)
+
+    res = conn.getresponse()
+    data = res.read()
+    print(data.decode("utf-8"))
